19357ab9bf293e6a8e9e2a46fda2e851b55f4777
Fady Fawzy <FadyFawzyOfficial@gmail.com>
2020-12-07 21:00:02 +0200

    17. Working with ListView

--- lib/widgets/transation_list.dart
+++ lib/widgets/transation_list.dart
@@ -10,60 +10,64 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        ...transactions
-            .map(
-              (transaction) => Card(
-                child: Row(
-                  children: [
-                    Container(
-                      child: Text(
-                        '\$${transaction.amount}',
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.purple,
-                        ),
-                      ),
-                      margin: EdgeInsets.symmetric(
-                        vertical: 10,
-                        horizontal: 15,
-                      ),
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          width: 2,
-                          color: Colors.purple,
-                        ),
-                      ),
-                      padding: EdgeInsets.all(10),
+    return Container(
+      height: 300,
+      child: ListView.builder(
+        itemBuilder: (
+          BuildContext context,
+          int index,
+        ) {
+          Transaction currentTransaction = transactions[index];
+          return Card(
+            elevation: 5,
+            child: Row(
+              children: [
+                Container(
+                  child: Text(
+                    '\$${currentTransaction.amount}',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.purple,
                     ),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          transaction.title,
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        Text(
-                          DateFormat.yMMMMd().format(transaction.data),
-                          style: TextStyle(
-                            color: Colors.grey,
-                          ),
-                        ),
-                      ],
-                    )
-                  ],
+                  ),
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 15,
+                  ),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      width: 2,
+                      color: Colors.purple,
+                    ),
+                  ),
+                  padding: EdgeInsets.all(10),
                 ),
-                elevation: 5,
-              ),
-            )
-            .toList()
-      ],
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      currentTransaction.title,
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      DateFormat.yMMMMd().format(currentTransaction.data),
+                      style: TextStyle(
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          );
+        },
+        itemCount: transactions.length,
+      ),
     );
   }
 }
